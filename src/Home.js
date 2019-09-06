7111e51be5592b9f7cb78b5604deeed382afd9e3
Luis Deschamps Rudge <luis@luisrudge.net>
2019-09-06 17:22:02 -0500

    aula29

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,29 @@
 import React from "react";
 import "./Home.css";
+import Logo from "./Logo";
+import HomeContentCard from "./HomeContentCard";
 
-const Home = () => <div>Home</div>;
+const Home = () => (
+  <div className="home">
+    <div className="home-image">
+      <div className="home-logo">
+        <Logo />
+      </div>
+    </div>
+    <div className="home-content-grid">
+      <HomeContentCard
+        title="Torça pelo seu time"
+        text="Veja os resultados do seu time favorito"
+      />
+      <HomeContentCard
+        title="Torça pelo seu time"
+        text="Veja os resultados do seu time favorito"
+      />
+      <HomeContentCard
+        title="Torça pelo seu time"
+        text="Veja os resultados do seu time favorito"
+      />
+    </div>
+  </div>
+);
 export default Home;
