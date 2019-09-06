86ee5129eac419ae1c68b7fd1f40e79fa224059b
Luis Deschamps Rudge <luis@luisrudge.net>
2019-09-06 16:28:07 -0500

    Aula27

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { useMediaQuery } from "react-responsive";
 import "./NavBar.css";
 
 const links = [
@@ -18,30 +18,39 @@ const links = [
 ];
 
 const NavBar = () => {
+  const isBigEnough = useMediaQuery({ query: "(min-width: 575px)" });
+
   const [open, setOpen] = React.useState();
+  const appLinks = links.map(l => (
+    <a key={l.to} href={l.to} className="nav-link">
+      {l.text}
+    </a>
+  ));
   return (
     <nav className="nav-bar">
-      <div className="nav-small">
-        <button
-          className="nav-small__button"
-          onClick={() => setOpen(o => !o)}
-          style={{
-            borderBottom: `1px solid ${open ? "#fff" : "transparent"}`
-          }}
-        >
-          MENU
-        </button>
-        <div
-          className="nav-small__links"
-          style={{ display: open ? "block" : "none" }}
-        >
-          {links.map(l => (
-            <a key={l.to} href={l.to} className="nav-link">
-              {l.text}
-            </a>
-          ))}
+      {isBigEnough ? (
+        <div className="nav-big">
+          {appLinks}
         </div>
-      </div>
+      ) : (
+        <div className="nav-small">
+          <button
+            className="nav-small__button"
+            onClick={() => setOpen(o => !o)}
+            style={{
+              borderBottom: `1px solid ${open ? "#fff" : "transparent"}`
+            }}
+          >
+            MENU
+          </button>
+          <div
+            className="nav-small__links"
+            style={{ display: open ? "block" : "none" }}
+          >
+            {appLinks}
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
