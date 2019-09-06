44a11588cdd5c3e433a122798d2a5e06b3845ad7
Luis Deschamps Rudge <luis@luisrudge.net>
2019-09-06 17:43:23 -0500

    aula30

--- src/Brasileirao.js
+++ src/Brasileirao.js
@@ -1,5 +1,35 @@
 import React from "react";
 import "./Brasileirao.css";
+import { getLeagueTable, getNextMatches } from "./api";
 
-const Brasileirao = () => <div>Brasileirao</div>;
+const Brasileirao = () => {
+  const [loading, setLoading] = React.useState(false);
+  const [leagueTable, setLeagueTable] = React.useState();
+  const [nextMatches, setNextMatches] = React.useState();
+  React.useEffect(() => {
+    const leagueId = 2013;
+    const load = async () => {
+      setLoading(true);
+      const promises = [getLeagueTable(leagueId), getNextMatches(leagueId)];
+      const [leagueTable, nextMatches] = await Promise.all(promises);
+      setLeagueTable(leagueTable.standings[0].table);
+      setNextMatches(nextMatches.matches);
+      setLoading(false);
+    };
+    load();
+  }, []);
+  return (
+    <div>
+      {loading && <h1>Carregando</h1>}
+      {!loading && leagueTable && (
+        <>
+          <h1>Tabela</h1>
+          <div></div>
+          <h1>Próximas partidas</h1>
+          <div></div>
+        </>
+      )}
+    </div>
+  );
+};
 export default Brasileirao;
